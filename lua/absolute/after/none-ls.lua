ae7209b8d0890b49c0bddb7b8d60a7bba1752adb
David Ibia <david.ibia@boxmarshall.com>
2024-01-16 11:20:10 +0100

    chore(none-ls.lua): update formatting sources in null-ls setup
    The formatting sources in the null-ls setup have been updated. The `null_ls.builtins.formatting.black` and `null_ls.builtins.formatting.rustfmt` sources have been removed, and the `null_ls.builtins.formatting.autopep8` source has been added. This change was made to improve the formatting capabilities of the null-ls plugin.

--- lua/absolute/after/none-ls.lua
+++ lua/absolute/after/none-ls.lua
@@ -1,11 +1,10 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.rustfmt,
-	},
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.autopep8,
+    },
 })
