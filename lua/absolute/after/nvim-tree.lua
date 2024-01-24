707bc2a20451e5bfec62b4eb535e414cdeecfce7
David Ibia <david.ibia@boxmarshall.com>
2024-01-24 15:54:34 +0100

    chore(nvim-tree.lua): reformat and organize the configuration options for nvim-tree plugin for better readability and maintainability

--- lua/absolute/after/nvim-tree.lua
+++ lua/absolute/after/nvim-tree.lua
@@ -9,39 +9,41 @@ vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
 vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
 
 nvimtree.setup({
-    view = {
-        width = 45,
-        relativenumber = true,
-    },
-    renderer = {
-        indent_markers = {
-            enable = true,
-        },
-        icons = {
-            glyphs = {
+	view = {
+		width = 45,
+		relativenumber = true,
+	},
+	update_focused_file = {
+		enable = true,
+	},
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
+		icons = {
+			glyphs = {
 
-                folder = {
-                    arrow_closed = "", -- arrow when folder is closed
-                    arrow_open = "", -- arrow when folder is open
-                }
-            }
-        }
-    },
-    -- disable window_picker for
-    -- explorer to work well with
-    -- window splits
-    actions = {
-        open_file = {
-            window_picker = {
-                enable = false,
-            },
-        },
-    },
-    filters = {
-        custom = { ".DS_Store" },
-    },
-    git = {
-        ignore = false,
-    }
-
+				folder = {
+					arrow_closed = "", -- arrow when folder is closed
+					arrow_open = "", -- arrow when folder is open
+				},
+			},
+		},
+	},
+	-- disable window_picker for
+	-- explorer to work well with
+	-- window splits
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	filters = {
+		custom = { ".DS_Store" },
+	},
+	git = {
+		ignore = false,
+	},
 })
