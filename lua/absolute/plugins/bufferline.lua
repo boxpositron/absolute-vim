dce8aaa3bc9699d9f753a9c0467b388132290639
David Ibia <david.ibia@boxmarshall.com>
2024-02-26 14:36:29 +0100

    chore(bufferline.lua): remove bufferline configuration and key mappings for tab management to simplify and streamline the setup of the bufferline plugin chore(bufferline.lua): remove bufferline plugin configuration and dependencies to declutter the plugin setup and improve maintainability

--- lua/absolute/plugins/bufferline.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
-    config = function()
-        require("absolute.after.bufferline")
-    end
-}
