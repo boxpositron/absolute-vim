9f5022b61b32cb92b77feaeca3f13d2d4d89dde2
David Ibia <david.ibia@boxmarshall.com>
2024-03-10 12:20:47 +0100

    feat(init.lua): add additional paths to package.path to include luarocks modules installed in user's home directory

--- lua/absolute/init.lua
+++ lua/absolute/init.lua
@@ -1,3 +1,6 @@
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+
 require("absolute.core")
 require("absolute.lazy")
 require("absolute.helpers")
