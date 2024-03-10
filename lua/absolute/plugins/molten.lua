347718979cb9a77056b3223ea1b7e14b2e9c08b2
David Ibia <david.ibia@boxmarshall.com>
2024-03-10 12:21:22 +0100

    feat(molten.lua): add molten-nvim plugin configuration to enhance Neovim functionality

---
+++ lua/absolute/plugins/molten.lua
@@ -0,0 +1,17 @@
+return {
+	"benlubas/molten-nvim",
+	version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+	dependencies = { "3rd/image.nvim" },
+	build = ":UpdateRemotePlugins",
+	init = function()
+		vim.g.molten_auto_open_output = false
+		vim.g.molten_wrap_output = true
+		vim.g.molten_virt_text_output = true
+		vim.g.molten_virt_lines_off_by_1 = true
+		vim.g.molten_image_provider = "image.nvim"
+		vim.g.molten_output_win_max_height = 20
+	end,
+	config = function()
+		require("absolute.after.molten")
+	end,
+}
