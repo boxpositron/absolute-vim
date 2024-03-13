bbda0a77575e6f333ba5eb43d5c20cd3bf573181
David Ibia <david.ibia@boxmarshall.com>
2024-03-13 01:57:55 +0100

    style(init.lua): comment out unused vim.opt.guicursor setting for clarity feat(init.lua): change statuscolumn and cursorline settings to use vim.opt for consistency refactor(init.lua): remove redundant undodir and undofile settings as they are not needed

--- lua/absolute/core/init.lua
+++ lua/absolute/core/init.lua
@@ -1,9 +1,10 @@
-vim.opt.guicursor = ""
+-- vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.o.statuscolumn = "%s %l %r"
+vim.opt.statuscolumn = "%s %l %r"
+vim.opt.cursorline = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -16,9 +17,6 @@ vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
