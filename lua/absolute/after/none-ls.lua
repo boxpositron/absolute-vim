0241ccc8273af9cf3e33e28f0f9e4f7eb62a0280
David Ibia <david.ibia@boxmarshall.com>
2024-03-17 15:02:11 +0100

    feat(none-ls.lua): add mypy diagnostic tool with extra_args to specify python executable path based on environment variables

--- lua/absolute/after/none-ls.lua
+++ lua/absolute/after/none-ls.lua
@@ -6,5 +6,11 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.formatting.black,
+		null_ls.builtins.diagnostics.mypy.with({
+			extra_args = function()
+				local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+				return { "--python-executable", virtual .. "/bin/python3" }
+			end,
+		}),
 	},
 })
