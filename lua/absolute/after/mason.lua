b1737abd9d263ee86593d47c41340f880066ac91
David Ibia <david.ibia@boxmarshall.com>
2024-06-21 12:51:57 +0100

    feat(mason.lua): add 'astro' to the list of installed language servers to support Astro language in Mason configuration

--- lua/absolute/after/mason.lua
+++ lua/absolute/after/mason.lua
@@ -2,6 +2,7 @@ require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
         "tsserver",
+        "astro",
         "biome",
         "rust_analyzer",
         "html",
