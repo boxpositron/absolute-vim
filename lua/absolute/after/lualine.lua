a8403da7359ac6a86236b7d9ece9a5715213548f
David Ibia <david.ibia@boxmarshall.com>
2024-07-29 01:02:55 +0100

    chore(lualine.lua): comment out lualine_c and lualine_z sections to temporarily remove tmux status components from lualine configuration

--- lua/absolute/after/lualine.lua
+++ lua/absolute/after/lualine.lua
@@ -94,6 +94,22 @@ lualine.setup({
         theme = my_lualine_theme,
     },
     sections = {
+        -- lualine_c = {
+        --     -- ...other lualine components
+        --     {
+        --         require('tmux-status').tmux_windows,
+        --         cond = require('tmux-status').show,
+        --         padding = { left = 3 },
+        --     },
+        -- },
+        -- lualine_z = {
+        --     -- ...other lualine components
+        --     {
+        --         require('tmux-status').tmux_session,
+        --         cond = require('tmux-status').show,
+        --         padding = { left = 3 },
+        --     },
+        -- },
         lualine_x = {
             {
                 GetFlutterToolsStatusLine,
