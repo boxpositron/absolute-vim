3f70665f56b7ab38ff0d692befacb65453c6d12f
David Ibia <david.ibia@boxmarshall.com>
2024-07-29 01:02:41 +0100

    feat(telescope.lua): add "telescope-themes" as a dependency to enhance the visual appearance of the telescope plugin

--- lua/absolute/plugins/telescope.lua
+++ lua/absolute/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-dap.nvim",
+        "andrew-george/telescope-themes",
     },
     config = function()
         require("absolute.after.telescope")
