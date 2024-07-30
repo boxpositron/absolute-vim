a101cf5e0cbc0dae6f4fb968e5c324536c1c9fc1
David Ibia <david.ibia@boxmarshall.com>
2024-07-30 23:21:58 +0100

    feat(trouble.lua): add key mappings for Trouble plugin to provide quick access to various functionalities like toggling diagnostics, symbols, LSP, location list, and quickfix list

--- lua/absolute/plugins/trouble.lua
+++ lua/absolute/plugins/trouble.lua
@@ -1,7 +1,38 @@
 return {
     "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("absolute.after.trouble")
-    end
+    keys = {
+        {
+            "<leader>td",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>tX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>ts",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>tl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>tL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>tq",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
 }
