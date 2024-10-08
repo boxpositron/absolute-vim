ae67eb68493142fedb131ded095cf42ca16049d5
David Ibia <david.ibia@boxmarshall.com>
2024-10-08 22:25:57 +0100

    feat(telescope.lua): add nvim-telescope/telescope-file-browser.nvim plugin to enhance file browsing capabilities

--- lua/absolute/plugins/telescope.lua
+++ lua/absolute/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-dap.nvim",
         "andrew-george/telescope-themes",
+        "nvim-telescope/telescope-file-browser.nvim",
     },
     config = function()
         require("absolute.after.telescope")
