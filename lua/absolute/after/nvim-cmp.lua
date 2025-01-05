e662865d4ef29c220c8c670c167c69e85c290356
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:34:47 +0100

    fix(nvim-cmp): change port variable case from lowercase port to uppercase PORT to improve semantics feat(nvim-cmp): add support for process.env.PORT environment variable to be able to run app on a configurable port

--- lua/absolute/after/nvim-cmp.lua
+++ lua/absolute/after/nvim-cmp.lua
@@ -1,5 +1,5 @@
 local cmp = require("cmp")
-local types = require('cmp.types')
+local types = require("cmp.types")
 local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
@@ -7,64 +7,65 @@ local lspkind = require("lspkind")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
-    completion = {
-        completeopt = "menu,menuone,preview,noinsert,noselect",
-        autocomplete = { types.cmp.TriggerEvent.TextChanged },
-    },
-    window = {
-        documentation = {
-            border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
-            winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
-        },
-        completion = {
-            side_padding = 1,
-            border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
-            winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
-            scrollbar = false,
-        },
-    },
-    snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-o>"] = cmp.mapping.complete(),                -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(),                   -- close completion menu
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- accept current selection
-    }),
-    -- sources for autocompletion
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" },  -- text within current buffer
-        { name = "path" },    -- file system paths,
-        { name = 'nvim_lsp_signature_help' },
-    }),
-    -- configure lspkind for vs-code like pictograms in completion menu
-    formatting = {
-        format = lspkind.cmp_format({
-            maxwidth = 50,
-            ellipsis_char = "...",
-        }),
-    },
+	completion = {
+		completeopt = "menu,menuone,preview,noinsert,noselect",
+		autocomplete = { types.cmp.TriggerEvent.TextChanged },
+	},
+	window = {
+		documentation = {
+			border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+			winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+		},
+		completion = {
+			side_padding = 1,
+			border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+			winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+			scrollbar = false,
+		},
+	},
+	snippet = { -- configure how nvim-cmp interacts with snippet engine
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-.>"] = cmp.mapping.complete(), -- show completion suggestions
+		["C-<BS>"] = cmp.mapping.abort(), -- close completion menu
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- accept current selection
+	}),
+
+	-- sources for autocompletion
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within current buffer
+		{ name = "path" }, -- file system paths,
+		{ name = "nvim_lsp_signature_help" },
+	}),
+	-- configure lspkind for vs-code like pictograms in completion menu
+	formatting = {
+		format = lspkind.cmp_format({
+			maxwidth = 50,
+			ellipsis_char = "...",
+		}),
+	},
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false },
 })
