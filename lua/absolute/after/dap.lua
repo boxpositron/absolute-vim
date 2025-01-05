f03a52f81a05b539da0292ddd5f8bdfac201e6f6
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:35:31 +0100

    fix(lua): remove unused imports and variables feat(lua): add support for debugpy package to enable debugging of python code

--- lua/absolute/after/dap.lua
+++ lua/absolute/after/dap.lua
@@ -1,33 +1,27 @@
 local dap = require("dap")
 local dapui = require("dapui")
-local dap_python = require("dap-python")
 local virtual_text = require("nvim-dap-virtual-text")
 
 -- Setup Virtual Text
 virtual_text.setup({
-    prefix = " ",
-    hl = "Comment",
-    lines = 3,
-    enabled = true,
+	prefix = " ",
+	hl = "Comment",
+	lines = 3,
+	enabled = true,
 })
 
-
-local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason")
-local debugpy_path = mason_path .. "/packages/debugpy/venv/bin/python"
-
 dapui.setup()
-dap_python.setup(debugpy_path)
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
+	dapui.open()
 end
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
+	dapui.close()
 end
 
 dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
+	dapui.close()
 end
 
 -- Setup Mappings
@@ -35,8 +29,3 @@ local opts = { noremap = true, silent = true }
 
 opts.desc = "Toggle breakpoint"
 vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint<CR>", opts)
-
-opts.desc = "Debug Python Run"
-vim.keymap.set("n", "<leader>dpr", function()
-    dap_python.test_method()
-end, opts)
