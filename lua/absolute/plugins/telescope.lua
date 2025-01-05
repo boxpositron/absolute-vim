bfa9f6386f5c61c402f07fdd757812f270f2babb
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:33:42 +0100

    feat(lua/absolute/plugins/telescope.lua): add telescope-live-grep-args.nvim plugin to telescope plugins list refactor(lua/absolute/plugins/telescope.lua): move telescope-dap.nvim and telescope-file-browser.nvim to the top of the list for better performance [INST:0]

--- lua/absolute/plugins/telescope.lua
+++ lua/absolute/plugins/telescope.lua
@@ -6,6 +6,12 @@ return {
         "nvim-telescope/telescope-dap.nvim",
         "andrew-george/telescope-themes",
         "nvim-telescope/telescope-file-browser.nvim",
+        {
+            "nvim-telescope/telescope-live-grep-args.nvim",
+            -- This will not install any breaking changes.
+            -- For major updates, this must be adjusted manually.
+            version = "^1.0.0",
+        }
     },
     config = function()
         require("absolute.after.telescope")
