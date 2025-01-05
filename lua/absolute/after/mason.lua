11a2e7125f171dde4945f37d1cba6714247ce595
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:35:09 +0100

    feat(lua): add support for null-ls linter and formatter refactor(lua): remove none-ls linter and formatter in favor of null-ls

--- lua/absolute/after/mason.lua
+++ lua/absolute/after/mason.lua
@@ -1,26 +1,26 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "ts_ls",
-        "astro",
-        "biome",
-        "rust_analyzer",
-        "html",
-        "emmet_ls",
-        "lua_ls",
-        'arduino_language_server',
-        'clangd',
-        "tailwindcss",
-        "svelte",
-        "cssls",
-        "jedi_language_server",
-        "kotlin_language_server"
-    },
-    automatic_installation = true,
+	ensure_installed = {
+		"ts_ls",
+		"astro",
+		"biome",
+		"rust_analyzer",
+		"html",
+		"emmet_ls",
+		"lua_ls",
+		"arduino_language_server",
+		"clangd",
+		"tailwindcss",
+		"svelte",
+		"cssls",
+		"jedi_language_server",
+		"kotlin_language_server",
+	},
+	automatic_installation = true,
 })
 require("mason-nvim-dap").setup({
-    ensure_installed = {
-        "debugpy", -- python
-    },
-    automatic_installation = true,
+	ensure_installed = {
+		"mypy",
+	},
+	automatic_installation = true,
 })
