3d1372ad47acbbe0cf15414b1c1d1cf93e8657e6
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:34:39 +0100

    fix(poet-v.lua): change auto activation to manual activation feat(poet-v.lua): add support for LSP restart after virtualenv activation

--- lua/absolute/after/poet-v.lua
+++ lua/absolute/after/poet-v.lua
@@ -1,22 +1,20 @@
 vim.g.poetv_executables = { "poetry", "pipenv" }
-vim.g.poetv_auto_activate = 1
+vim.g.poetv_auto_activate = 0
 vim.g.poetv_set_environment = 1
 vim.g.poetv_statusline_symbol = "🐍"
 
-
--- Keybindings
-
-local opts = { noremap = true, silent = true }
-
--- Poetv: Activate Virtualenv
-opts.desc = "Poetv: Activate virtualenv"
-vim.keymap.set("n", "<leader>pva", "<cmd>PoetvActivate<CR>", opts)
-
--- Poetv: Deactivate Virtualenv
-opts.desc = "Poetv: Deactivate virtualenv"
-vim.keymap.set("n", "<leader>pvd", "<cmd>PoetvDeactivate<CR>", opts)
-
 -- Setup Autocommands
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" },
-    { pattern = { "*/.venv/*", "*.py" }, callback = function() vim.cmd([[ PoetvActivate]]) end })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*/.venv/*", "*.py" },
+	callback = function()
+		-- check if poetv is activated
+
+		if vim.g.poetv_name ~= nil then
+			return
+		else
+			vim.cmd([[ PoetvActivate ]])
+			vim.cmd([[ LspRestart ]])
+		end
+	end,
+})
