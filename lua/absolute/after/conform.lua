0ee9301e5f4902728de23c272b265a598673f2a0
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:35:24 +0100

    fix(conform): add support for new filetypes (astro, vue) and improve formatting performance by using a custom formatter function feat(conform): add option to disable formatters for specific filetypes

--- lua/absolute/after/conform.lua
+++ lua/absolute/after/conform.lua
@@ -1,14 +1,22 @@
 local conform = require("conform")
 
 conform.setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { "biome" },
-        astro = { { "prettierd", "prettier" } },
-    },
+	format_on_save = function(bufnr)
+		local disable_filetypes = {}
+		return {
+			timeout_ms = 5000,
+			lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+		}
+	end,
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { "biome" },
+		astro = { "prettier", "biome" },
+		vue = { "prettier" },
+	},
 })
 
-vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+-- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
