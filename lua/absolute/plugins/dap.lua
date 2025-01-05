3afaff7f57a6eec6354bf673c65d1314e5ad4a82
David Ibia <david.ibia@boxmarshall.com>
2025-01-05 23:33:18 +0100

    fix(lua/absolute/plugins/dap.lua): remove unnecessary dependencies and config functions

--- lua/absolute/plugins/dap.lua
+++ lua/absolute/plugins/dap.lua
@@ -1,18 +1,12 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "theHamsta/nvim-dap-virtual-text",
-        "rcarriga/nvim-dap-ui",
-        "nvim-neotest/nvim-nio",
-        "nvim-telescope/telescope-dap.nvim",
-        {
-            "mfussenegger/nvim-dap-python",
-            ft = "python",
-            dependencies = { "mfussenegger/nvim-dap" },
-        },
-        -- "Pocco81/DAPInstall.nvim"
-    },
-    config = function()
-        require("absolute.after.dap")
-    end,
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"theHamsta/nvim-dap-virtual-text",
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"nvim-telescope/telescope-dap.nvim",
+	},
+	config = function()
+		require("absolute.after.dap")
+	end,
 }
