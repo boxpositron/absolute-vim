71cbbd49b986dfa7c3e0990ae22f7856d53c5c41
David Ibia <davidibia@Davids-MacBook-Pro.local>
2025-01-06 13:26:37 +0100

    feat: disabled apple-pkl plugin

--- lua/absolute/plugins/apple-pkl.lua
+++ lua/absolute/plugins/apple-pkl.lua
@@ -1,11 +1,12 @@
 return {
-	"https://github.com/apple/pkl-neovim",
-	lazy = true,
-	event = "BufReadPre *.pkl",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-	},
-	build = function()
-		vim.cmd("TSInstall! pkl")
-	end,
+    "https://github.com/apple/pkl-neovim",
+    enabled = false,
+    lazy = true,
+    event = "BufReadPre *.pkl",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    build = function()
+        vim.cmd("TSInstall! pkl")
+    end,
 }
