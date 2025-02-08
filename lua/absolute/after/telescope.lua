b39ddb92b124de5026b470751a0d3e3bcf5c4770
David Ibia <david.ibia@boxmarshall.com>
2025-02-09 00:11:11 +0100

    feat: use builtin grep and live grep with conditions

--- lua/absolute/after/telescope.lua
+++ lua/absolute/after/telescope.lua
@@ -4,83 +4,95 @@ local actions = require("telescope.actions")
 local trouble = require("trouble.sources.telescope")
 local file_browser_actions = require("telescope._extensions.file_browser.actions")
 local live_grep_shortcuts = require("telescope-live-grep-args.shortcuts")
+local live_grep_actions = require("telescope-live-grep-args.actions")
 
 telescope.setup({
-    defaults = {
-        vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-        },
-        mappings = {
-            i = {
-                ["<C-t>"] = trouble.open,
-                ["<C-q>"] = actions.send_to_qflist,
-            },
-            n = {
-                ["<C-t>"] = trouble.open,
-                ["<C-q>"] = actions.send_to_qflist,
-            },
-        },
-    },
-    extensions = {
-        file_browser = {
-            hijack_netrw = true,
-            hide_parent_dir = true,
-            depth = 1,
-            respect_gitignore = vim.fn.executable "fd" == 1,
-            hidden = { file_browser = true, folder_browser = false },
-            auto_depth = false,
-            browse_files = require("telescope._extensions.file_browser.finders").browse_files,
-            display_stat = {
-                date = true,
-                size = true,
-                mode = false,
-            },
-            use_fd = true,
-            git_status = true,
-            mappings = {
-                ["i"] = {
-                    ["<A-c>"] = file_browser_actions.create,
-                    ["<S-CR>"] = file_browser_actions.create_from_prompt,
-                    ["<A-r>"] = file_browser_actions.rename,
-                    ["<A-m>"] = file_browser_actions.move,
-                    ["<A-y>"] = file_browser_actions.copy,
-                    ["<A-d>"] = file_browser_actions.remove,
-                    ["<C-o>"] = file_browser_actions.open,
-                    ["<C-g>"] = file_browser_actions.goto_parent_dir,
-                    ["<C-e>"] = file_browser_actions.goto_home_dir,
-                    ["<C-w>"] = file_browser_actions.goto_cwd,
-                    ["<C-t>"] = file_browser_actions.change_cwd,
-                    ["<C-f>"] = file_browser_actions.toggle_browser,
-                    ["<C-h>"] = file_browser_actions.toggle_hidden,
-                    ["<C-s>"] = file_browser_actions.toggle_all,
-                    ["<bs>"] = file_browser_actions.backspace,
-                },
-                ["n"] = {
-                    ["c"] = file_browser_actions.create,
-                    ["r"] = file_browser_actions.rename,
-                    ["m"] = file_browser_actions.move,
-                    ["y"] = file_browser_actions.copy,
-                    ["d"] = file_browser_actions.remove,
-                    ["o"] = file_browser_actions.open,
-                    ["g"] = file_browser_actions.goto_parent_dir,
-                    ["e"] = file_browser_actions.goto_home_dir,
-                    ["w"] = file_browser_actions.goto_cwd,
-                    ["t"] = file_browser_actions.change_cwd,
-                    ["f"] = file_browser_actions.toggle_browser,
-                    ["h"] = file_browser_actions.toggle_hidden,
-                    ["s"] = file_browser_actions.toggle_all,
-                },
-            },
-        },
-    },
-})
+	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--follow",
+			"--hidden",
+			"--glob=!.git/",
+		},
+		mappings = {
+			i = {
+				["<C-t>"] = trouble.open,
+				["<C-q>"] = actions.send_to_qflist,
+			},
+			n = {
+				["<C-t>"] = trouble.open,
+				["<C-q>"] = actions.send_to_qflist,
+			},
+		},
+	},
+	extensions = {
+		live_grep_args = {
+			auto_quoting = false,
+			mappings = {
+				i = {
+					["C-space"] = live_grep_actions.to_fuzzy_refine,
+				},
+			},
+		},
 
+		file_browser = {
+			hijack_netrw = true,
+			hide_parent_dir = true,
+			depth = 1,
+			respect_gitignore = vim.fn.executable("fd") == 1,
+			hidden = { file_browser = true, folder_browser = false },
+			auto_depth = false,
+			browse_files = require("telescope._extensions.file_browser.finders").browse_files,
+			display_stat = {
+				date = true,
+				size = true,
+				mode = false,
+			},
+			use_fd = true,
+			git_status = true,
+			mappings = {
+				["i"] = {
+					["<A-c>"] = file_browser_actions.create,
+					["<S-CR>"] = file_browser_actions.create_from_prompt,
+					["<A-r>"] = file_browser_actions.rename,
+					["<A-m>"] = file_browser_actions.move,
+					["<A-y>"] = file_browser_actions.copy,
+					["<A-d>"] = file_browser_actions.remove,
+					["<C-o>"] = file_browser_actions.open,
+					["<C-g>"] = file_browser_actions.goto_parent_dir,
+					["<C-e>"] = file_browser_actions.goto_home_dir,
+					["<C-w>"] = file_browser_actions.goto_cwd,
+					["<C-t>"] = file_browser_actions.change_cwd,
+					["<C-f>"] = file_browser_actions.toggle_browser,
+					["<C-h>"] = file_browser_actions.toggle_hidden,
+					["<C-s>"] = file_browser_actions.toggle_all,
+					["<bs>"] = file_browser_actions.backspace,
+				},
+				["n"] = {
+					["c"] = file_browser_actions.create,
+					["r"] = file_browser_actions.rename,
+					["m"] = file_browser_actions.move,
+					["y"] = file_browser_actions.copy,
+					["d"] = file_browser_actions.remove,
+					["o"] = file_browser_actions.open,
+					["g"] = file_browser_actions.goto_parent_dir,
+					["e"] = file_browser_actions.goto_home_dir,
+					["w"] = file_browser_actions.goto_cwd,
+					["t"] = file_browser_actions.change_cwd,
+					["f"] = file_browser_actions.toggle_browser,
+					["h"] = file_browser_actions.toggle_hidden,
+					["s"] = file_browser_actions.toggle_all,
+				},
+			},
+		},
+	},
+})
 
 telescope.load_extension("live_grep_args")
 telescope.load_extension("file_browser")
@@ -89,34 +101,35 @@ telescope.load_extension("dap")
 telescope.load_extension("themes")
 
 local find_files = function()
-    builtin.find_files({
-        find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
-    })
+	builtin.find_files({
+		find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
+	})
 end
 
 local default_grep_files = function()
-    local input = vim.fn.input("Global Search > ")
-    local trimmed_input = vim.fn.trim(input)
+	local input = vim.fn.input("[Global Search]: ")
+	local trimmed_input = vim.fn.trim(input)
 
-    -- Check the length of the input
+	-- Check the length of the input
 
-    -- If the input is empty, return
+	local live_grep_args = telescope.extensions.live_grep_args
+	-- If the input is empty open the search prompt
 
-    if trimmed_input == "" then
-        return
-    end
+	if trimmed_input == "" then
+		live_grep_args.live_grep_args()
+		return
+	end
 
-    local live_grep_args = telescope.extensions.live_grep_args
-
-    live_grep_args.live_grep_args({
-        default_text = trimmed_input,
-    })
+	builtin.grep_string({ search = trimmed_input })
 end
 
 -- Function to get the visually selected text
 
 -- Pipe the visual selection into Telescope find_files
 local opts = { noremap = true, silent = true }
+
+opts.desc = "Fuzzy find themes"
+vim.api.nvim_set_keymap("n", "<leader>th", "<cmd>Telescope themes<CR>", opts)
 
 opts.desc = "Open file browser (cwd)"
 vim.keymap.set("n", "<leader>pf", "<cmd>Telescope file_browser<CR>", opts)
