24c6466d7b963ed0fe464cc018daa0c8c740e478
David Ibia <david.ibia@boxmarshall.com>
2025-03-08 15:57:15 +0100

    feat: disabled extension

--- lua/absolute/plugins/nvim-surround.lua
+++ lua/absolute/plugins/nvim-surround.lua
@@ -1,8 +1,9 @@
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("absolute.after.nvim-surround")
-    end,
+	"kylechui/nvim-surround",
+	enabled = false,
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("absolute.after.nvim-surround")
+	end,
 }
